0416f247cd941ca2e3f5f5e2477b804333d11310
Reese <65929791+bettameta@users.noreply.github.com>
2023-01-17 13:40:01 -0800

    Updated plugins and made some changes to CSS and HTML in the theme editor.

--- wp-content/themes/expo/404.php
+++ wp-content/themes/expo/404.php
@@ -11,12 +11,12 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'expo' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'Error 404! That page can&rsquo;t be found.', 'expo' ); ?></h1>
+
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'expo' ); ?></p>
-
+					<p><?php _e( 'Try searching the site.', 'expo' ); ?></p>
 					<?php get_search_form(); ?>
 
 				</div><!-- .page-content -->
